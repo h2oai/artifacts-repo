ef4582ce143b6c33940e2e2200a518929f7fb450
h2o-ops <h2o-ops@h2o.ai>
2020-12-04 05:41:08 +0000

    Adding manual

---
+++ dai/README.sh
@@ -0,0 +1,15 @@
+Driverless AI 1.9.0.4
+=====================
+
+To get `dai-docker-centos7-x86_64-1.9.0.4-10.0.tar.gz` please run
+
+```
+./combine.sh
+```
+
+or
+
+```
+cat x*PART > dai-docker-centos7-x86_64-1.9.0.4-10.0.tar.gz
+sha1sum -c dai-docker-centos7-x86_64-1.9.0.4-10.0.tar.gz.sha1
+```
